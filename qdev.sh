28859718919c7be3339eec5188ac5d1e3085d422
Kasper Nielsen <kasperni@gmail.com>
2023-12-07 06:19:55 +0000

    update

---
+++ qdev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#NIORD_HOME=~/.niord-dk
+NIORD_HOME=home-niord-dk
+
+#rm -fr $NIORD_HOME
+# Check if the NIORD_HOME directory does not exist
+if [ ! -d "$NIORD_HOME" ]; then
+    echo "Creating Niord Home"
+    mkdir -p $NIORD_HOME/batch-jobs/batch-sets
+    cp niord-dk-dev-basedata.zip $NIORD_HOME/batch-jobs/batch-sets/
+fi
+
+
+echo "Starting Niord DK"
+
+mvn -o quarkus:dev -Dniord.home=../$NIORD_HOME -Dquarkus.jaeger.enabled=true
